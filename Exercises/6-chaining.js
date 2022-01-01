9d37da460842cab9084248d9745b6cff70b09e1f
Alexander <58190800+Alexander-pixel@users.noreply.github.com>
2022-01-01 15:09:11 +0200

    Update 6-chaining.js

--- Exercises/6-chaining.js
+++ Exercises/6-chaining.js
@@ -4,12 +4,23 @@
 const EXPECTED_PIN = '4967';
 const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 
-// Impement function press
+// Implement function press
 // that allows to enter pin code by one character,
 // e.g. press('3').press('4').press('5').press('6')
 //
 // For hint use https://github.com/HowProgrammingWorks/Cheatsheet
 
-const press = null;
+const press = (initial) => ({
+  value: initial,
+  arr: [initial],
+  press(value) {
+    this.arr.push(value);
+    if (this.arr.length === EXPECTED_PIN.length) return checkPin(...this.arr);
+    return this;
+  }
+});
+
+const add = press('4').press('9').press('6').press('7');
+console.log(add);
 
 module.exports = { press };
